e9cd5a2ef51e399b6ded1b078227eac85a7c0332
Sukhdev Yadav <54099857+sukhdevyadav3@users.noreply.github.com>
2026-02-28 18:51:04 +0530

    fix: suppress browser extension hydration warnings on root html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
   const session = await getSession();
 
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
