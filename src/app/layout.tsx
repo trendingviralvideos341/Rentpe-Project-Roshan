d9d27c9a6562c7ef1db8cc7bf2d5e1531e517a10
Roshan <trendingviralvideos341@gmail.com>
2026-03-14 01:29:06 +0530

    chore: cleanup deployment verification comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// Deployment Verification: GitHub Migration to Roshan Account Successful
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
