641c311af0092d2bb6768fd6a14d0a87aecd93c9
Roshan <trendingviralvideos341@gmail.com>
2026-07-18 16:26:17 +0530

    fix(lint): resolve empty skeleton interface issue

--- src/components/ui/DashboardSkeleton.tsx
+++ src/components/ui/DashboardSkeleton.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { cn } from "@/lib/utils";
 
-interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-function Skeleton({ className, ...props }: SkeletonProps) {
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-muted/50", className)}
